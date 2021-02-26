5162aad6e6273380905bd9bd03662efa9f95249e
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-02-26 22:39:08 +0330

    because git rm -rf --chache i should add all these to git

---
+++ userValidation/getuser/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse
+from .models import GettingUser
+
+
+# Create your views here.
+def signUp(request):
+    '''this method is for sign up users in app'''
+    return HttpResponse('Hello, World!')
