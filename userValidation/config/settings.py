cc13d6dcb09827ae3c1114edeff2f3b87b99796c
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-02-26 18:53:48 +0330

    signup model created

--- userValidation/config/settings.py
+++ userValidation/config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'getuser.apps.GetuserConfig',
 ]
 
 MIDDLEWARE = [
--- userValidation/getuser/views.py
+++ userValidation/getuser/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from models import GettingUser
+
 
 # Create your views here.
+def signUp(request):
+    pass
+        
