295084eafcbd0b41242501016948fc6f3513f9ee
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-02-26 22:27:22 +0330

    model to Model in braces

--- userValidation/getuser/models.py
+++ userValidation/getuser/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
 class GettingUser(models.Model):
-    name=models.CharField(max_length=200)
-    email=models.EmailField()
-    username=models.CharField(max_length=100)
-    password=models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
 
     def __str__(self):
         return self.username
