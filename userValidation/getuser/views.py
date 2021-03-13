87c6ddfb665c3428fcd035b64ba9235779504a66
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-03-13 13:43:24 +0330

    add render method for method

--- userValidation/getuser/views.py
+++ userValidation/getuser/views.py
@@ -5,4 +5,5 @@ from .models import GettingUser
 # Create your views here.
 def signUp(request):
     '''this method is for sign up users in app'''
-    return HttpResponse('Hello, World!')
+    # return HttpResponse('Hello, World!')
+    return render(request, 'index.html')
