3f96e48764c707084bc380b72d29e0d93de905d1
MohammadJS2642 <mohammadjs2642@gmail.com>
2021-03-14 20:10:09 +0330

    added inside dirs in templates session os.path.join(BASE_DIR, 'template')

--- userValidation/config/settings.py
+++ userValidation/config/settings.py
@@ -64,10 +64,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'config.urls'
 
+
+# added in DIRS (os.path.join(BASE_DIR, 'templates'))
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [(os.path.join(BASE_DIR, 'templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
